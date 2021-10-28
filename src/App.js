7c9ebdcbd2f90ec2dd5838e04b295ec7533ce011
Nguyen Trung Tai - TJ <tai.nguyentrung1996@gmail.com>
2021-10-28 14:18:57 +0700

    Add UI User

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react"
 import TopBar from "./components/topbar/TopBar"
 import Sidebar from "./components/sidebar/Sidebar"
 import Home from "./pages/home/Home"
+import UserList from "./pages/userList/UserList"
+import User from "./pages/user/User"
+import NewUser from "./pages/newUser"
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
-
 import "./app.css"
-import UserList from "./pages/home/userList/UserList"
 
 function App() {
   return (
@@ -21,6 +22,12 @@ function App() {
 
           <Route path="/users">
             <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
           </Route>
         </Switch>
       </div>
