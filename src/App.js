914fb9aaeab8652340aeb4c3383ade777c5885ca
Nguyen Trung Tai - TJ <tai.nguyentrung1996@gmail.com>
2021-10-29 21:34:05 +0700

    Add ProductLiost, NewProduct, EditProduct UI

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Sidebar from "./components/sidebar/Sidebar"
 import Home from "./pages/home/Home"
 import UserList from "./pages/userList/UserList"
 import User from "./pages/user/User"
-import NewUser from "./pages/newUser"
+import NewUser from "./pages/newUser/NewUser"
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "./app.css"
+import ProductList from "./pages/productList/ProductList"
+import Product from "./pages/product/Product"
+import NewProduct from "./pages/newProduct/NewProduct"
 
 function App() {
   return (
@@ -28,6 +31,15 @@ function App() {
           </Route>
           <Route path="/newUser">
             <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <NewProduct />
           </Route>
         </Switch>
       </div>
